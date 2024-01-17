f751ca65d66132295d72a1914e68233abef43bca
Mudassar Saleem <64350255+Mudassar61Hashmi@users.noreply.github.com>
2024-01-17 22:42:22 +0000

    This is just for commit

---
+++ 01_basics/03_conversionOperations.js
@@ -0,0 +1,44 @@
+let score= "Mudassar"
+
+// console.log(score)
+
+let valueNumber= Number(score)
+// console.log(valueNumber)
+
+let testBool= 0
+let checkBool= Boolean(testBool)
+
+// console.log(checkBool)
+
+let anyNumber= 45
+let stringNum= String(anyNumber)
+// console.log(typeof stringNum)
+
+let value= 36
+let negVal= -value
+// console.log(negVal)
+
+let str1= "Hello"
+let str2= "Mudassar"
+let str3= str1+str2
+// console.log(str3)
+
+// console.log(1+"2")
+// console.log(1+1+"2")
+
+// console.log((3+4)*5%3)
+
+// console.log(1+true)
+// console.log(+true)
+// console.log("1"+true)
+// console.log(+"")
+
+let num1, num2, num3
+num1=num2=num3=2+2
+// console.table([num1,num2,num3])
+
+let counter= 200
+++counter;
+console.log(counter)
+
+
