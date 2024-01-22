ae00d5182d7c28114243d3942cf64dd09f0d8e22
Mudassar Saleem <64350255+Mudassar61Hashmi@users.noreply.github.com>
2024-01-22 22:44:35 +0000

    strings

--- 01_basics/05_strings.js
+++ 01_basics/05_strings.js
@@ -1,8 +1,35 @@
-const name="Mudassar Hashmi"
+const name= "Mudassar Hashmi"
 const age= 21
- console.log(`My ${name} & i am ${age} years old `)
+// console.log(`My name is ${name} & my age is ${age}`)
+const newName= new String("hashmi-g")
+// console.log(newName);
 
- const uniqueName= new String("myName")
- console.log(uniqueName)
+// console.log(newName.__proto__)
+// console.log(newName[3])
 
- console.log()
+// console.log(newName.toUpperCase())
+// console.log(newName.length)
+// console.log(newName.charAt(4))
+// console.log(newName.indexOf("m"))
+
+// const newString= newName.substring(2,4)
+// console.log(newString)
+
+// const anotherString= newName.slice(3,5)
+// console.log(anotherString)
+
+// const spaceString= "        hashmi48     ";
+// console.log(spaceString.trim())
+
+// const boldedString= "My Name is Hashmi"
+// console.log(boldedString.concat("G"))
+
+// const str= "hashmi@gmail.com"
+// const str2= "@"
+// const str3=".com"
+// console.log(str.search(str2))
+// console.log(str.search(str3))
+
+const obj= new String("Hello")
+console.log(obj)
+console.log(obj.toString())
