d22278747237195c3dee6b22e5164acf43c43c01
Mudassar Saleem <64350255+Mudassar61Hashmi@users.noreply.github.com>
2024-01-22 21:48:49 +0000

    strings

---
+++ 01_basics/05_strings.js
@@ -0,0 +1,8 @@
+const name="Mudassar Hashmi"
+const age= 21
+ console.log(`My ${name} & i am ${age} years old `)
+
+ const uniqueName= new String("myName")
+ console.log(uniqueName)
+
+ console.log()
