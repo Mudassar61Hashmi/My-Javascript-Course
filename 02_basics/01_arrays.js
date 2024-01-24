a9e739126318ac95eaad52160ca7e504bcbe6f21
Mudassar Saleem <64350255+Mudassar61Hashmi@users.noreply.github.com>
2024-01-24 20:38:29 +0000

    arrays_01

---
+++ 02_basics/01_arrays.js
@@ -0,0 +1,33 @@
+const myArr= [1,2,3,4,5]
+// console.log(typeof myArr);
+
+// console.log(myArr[2]);
+
+const myArr1= new Array("Test1","Dani","Mudassar","Hamza")
+// console.log(myArr1);
+
+//-------------------Arrays Methods-------------------
+
+// myArr1.push("Jawad","Haris")
+// console.log(myArr1);
+
+// myArr1.pop(2)
+// console.log(myArr1);
+
+// myArr1.shift()
+// myArr1.unshift("Add New","New 2")
+// console.log(myArr1);
+
+// const newArr= myArr1.join()
+// console.log(newArr);
+
+// const test1= myArr1.length
+// console.log(test1);
+
+// const myn1= myArr1.slice(0,2)
+// console.log("A", myArr1);
+// console.log(myn1);
+
+console.log("B", myArr1);
+const myn2= myArr1.splice(0,2)
+console.log(myn2);
