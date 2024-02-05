f3cf81a16ae2fd810f91b63fdaabdb3ecaac9a13
Mudassar Saleem <64350255+Mudassar61Hashmi@users.noreply.github.com>
2024-02-05 21:43:40 +0000

    ArrowFunc

---
+++ 03_basics/03_Arrows.js
@@ -0,0 +1,16 @@
+// const anyFunc= ()=>{
+//     let userName= "Mudassar Hashmi"
+//     console.log(userName);
+// }
+// anyFunc()
+
+// const addnums= (num1,num2)=>{
+// return num1+num2
+// }
+
+// console.log(addnums(3,4))
+
+/*Implicit function */
+
+const addTwo= (num1,num2)=>(num1+num2)
+console.log(addTwo(3,4))
