d991dcef7ed5a2db8b1183403fb677b7e91bfc4d
Mudassar Saleem <64350255+Mudassar61Hashmi@users.noreply.github.com>
2024-02-05 21:52:16 +0000

    IIFE

---
+++ 03_basics/04_iife.js
@@ -0,0 +1,17 @@
+/*IIFE stands for immdiately invoked function expressions */
+
+/*Syntax of IIFE 
+(function functionName(params){
+
+})()
+*/
+
+(function anyFunc(){
+let userName="Mudassar"
+console.log(userName)
+})();
+/* Use multiple IIFE then use semicolon at last parenthesis */
+(function anotherFunc(){
+let userName="Hashmi"
+console.log(userName)
+})()
