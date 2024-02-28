58ca8183033c964488cb31898306b2e21d03d753
Mudassar Saleem <64350255+Mudassar61Hashmi@users.noreply.github.com>
2024-02-28 21:49:13 +0000

    switch

---
+++ 04_control-flow/switch.js
@@ -0,0 +1,16 @@
+const month= "feb"
+
+switch (month) {
+    case "jan":
+        alert("You select january")
+        break;
+case "mar":
+    console.log("You Select march")
+    break;
+    case "feb":
+        console.log("You Select february")
+        break;
+    default:
+        console.log("Invalid data")
+        break;
+}
