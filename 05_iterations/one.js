55ed1ec2193f1bcd0a62f02bf9a07d2f0acde171
Mudassar Saleem <64350255+Mudassar61Hashmi@users.noreply.github.com>
2024-03-04 23:20:58 +0000

    one

---
+++ 05_iterations/one.js
@@ -0,0 +1,26 @@
+// for (let i=1; i<=10; i++) {
+//    const elem= i;
+//    if(elem==5){
+//     console.log(`5 is best number`)
+//     break;
+//    }
+//    console.log(elem)
+// }
+
+// for (let tab=1; tab<=10; tab++) {
+//     console.log(`5 x ${tab} = ${5*tab}`)
+    
+// }
+
+// for (let i=1; i<=5; i++) {
+//     for (let j=1; j<=5; j++) {
+//        console.log(`Outer Loop ${i} and Inner Loop ${j}`)
+        
+//     }
+    
+// }
+let fruits= ["Apple", "Mango","Grapes"]
+for (let index = 0; index < fruits.length; index++) {
+    const element = fruits[index];
+    console.log(element)
+}
