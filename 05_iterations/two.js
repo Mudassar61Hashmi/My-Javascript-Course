da71c5af7b8cb53b9d912957f2d60aebb3704280
Mudassar Saleem <64350255+Mudassar61Hashmi@users.noreply.github.com>
2024-03-06 22:01:48 +0000

    two

---
+++ 05_iterations/two.js
@@ -0,0 +1,18 @@
+// let i= 1;
+// while (i<=10) {
+//     console.log(i)
+//     i++;
+// }
+
+// let num=1;
+// while (num<=10) {
+//     console.log(`5 x ${num} = ${5*num}`);
+//     num++;
+// }
+
+let myArray= ["Test1","Test2","Test3","Test4"];
+let arr=0;
+while (arr<myArray.length) {
+    console.log(myArray[arr]);
+    arr++;
+}
