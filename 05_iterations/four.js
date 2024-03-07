bfa133fb60d264027486888d5a01335f56210bf6
Mudassar Saleem <64350255+Mudassar61Hashmi@users.noreply.github.com>
2024-03-07 21:40:25 +0000

    four

---
+++ 05_iterations/four.js
@@ -0,0 +1,19 @@
+// const myObject={
+//     js: "JavaScript",
+//     cpp: "C Plus Plus",
+//     py: "Python"
+// }
+
+// for (const key in myObject) {
+//     console.log(`The ${key} Extension for ${myObject[key]}.`) 
+//     }
+
+const map= new Map()
+map.set('PK',"Pakistan")
+map.set('SA',"Saudia Arbia")
+map.set('IND',"India")
+map.set('ENG',"England")
+
+for (const key in map) {
+    console.log(map)
+}
